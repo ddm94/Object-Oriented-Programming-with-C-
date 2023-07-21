43477e8d16c13cfd51e200cb945755b62197246d
Daniele Di Modica <d.dimo94@hotmail.com>
2023-07-21 14:52:50 +0100

    Add Character class

---
+++ oop-adventure/Src/Character.cs
@@ -0,0 +1,20 @@
+﻿
+namespace OOPAdventure
+{
+    /// <summary>
+    /// An abstract class can not be instantiated
+    /// This is a base class that other classes
+    /// will extend off of in order to get their logic
+    /// </summary>
+    public abstract class Character
+    {
+        // Property
+        public string Name { get; set; }
+
+        // The constructor is called when a class is created
+        public Character(string name)
+        {
+            Name = name;
+        }
+    }
+}
