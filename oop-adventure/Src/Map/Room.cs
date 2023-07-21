f8d144511571e32e169995e954ccf1c7c0f60c2b
Daniele Di Modica <d.dimo94@hotmail.com>
2023-07-21 16:12:14 +0100

    Room
    Each House instance can have multiple Room and each Room will have its own unique name, location and items.

---
+++ oop-adventure/Src/Map/Room.cs
@@ -0,0 +1,22 @@
+﻿
+namespace OOPAdventure
+{
+    public class Room
+    {
+        public string Name { get; set; } = Text.Language.DefaultName;
+        public string Description { get; set; } = Text.Language.DefaultRoomDescription;
+
+        // Represents all the neighbours that this room can have
+        public Dictionary<Directions, int> Neighbours { get; set; } = new()
+        {
+            // Initially set to -1 to let us know that there is no neighbour in that direction
+            {Directions.North, -1},
+            {Directions.South, -1},
+            {Directions.East, -1},
+            {Directions.West, -1},
+            {Directions.None, -1},
+        };
+
+        public bool Visited { get; set; }
+    }
+}
--- oop-adventure/Src/Text/Language.cs
+++ oop-adventure/Src/Text/Language.cs
@@ -10,5 +10,7 @@ namespace OOPAdventure
         public string Welcome { get; protected set; } = "";
         public string ChooseYourName { get; protected set; } = "";
         public string DefaultName { get; protected set; } = "";
+        public string DefaultRoomName { get; protected set; } = "";
+        public string DefaultRoomDescription { get; protected set; } = "";
     }
 }
