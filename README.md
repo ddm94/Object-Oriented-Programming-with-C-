7a8d5f6586a27d59e981d24d6c3457bf3807fe1d
Daniele Di Modica <d.dimo94@hotmail.com>
2023-07-21 14:32:38 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Object-Oriented-Programming-with-Csharp
+ An interactive text adventureg ame in C# and .NET 6
---
+++ oop-adventure/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Hello, what is your name?");
+
+var name = Console.ReadLine();
+
+if (name == String.Empty)
+{
+    name = "No Name";
+}    
+
+// {0} - Here name will automatically replace the 0 token
+Console.WriteLine("Welcome {0} to your OOP Adventure!",  name);
