1308b2c6d8aa2ef5b8d03d281f9f9d534451bb29
Daniele Di Modica <d.dimo94@hotmail.com>
2023-07-21 16:36:26 +0100

    Implement Actions logic
    - Add abstract class Action
    - Add singleton class Actions

---
+++ oop-adventure/Src/Actions/Actions.cs
@@ -0,0 +1,29 @@
+﻿
+namespace OOPAdventure
+{
+    // Sealed - this class is unmodifiable
+    // Adds another layer of protection to ensure
+    // that other classes can not tamper with
+    // or extend from classes defined as Singleton
+    public sealed class Actions
+    {
+        private static Actions _instance;
+
+        public static Actions Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Actions();
+
+                return _instance;
+            }
+        }
+
+        private readonly Dictionary<string, Action> _registeredActions = new();
+
+        // Private constructor to ensure there is no way
+        // to create  an instance of this class outside of the instance instantiator.
+        private Actions() { } 
+    }
+}
