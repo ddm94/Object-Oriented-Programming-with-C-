e455f26c9ac74757b8d8da7d87bc67d9e76d6792
Daniele Di Modica <d.dimo94@hotmail.com>
2023-07-21 15:45:50 +0100

    Add Singleton Text class
    Remove reference to Language inside Program and replace with Text, effectively creating a single way of accessing text throughout the game.

--- oop-adventure/Src/Text/English.cs
+++ oop-adventure/Src/Text/English.cs
@@ -6,6 +6,8 @@ namespace OOPAdventure
         public English()
         {
             ChooseYourName = "Hello, what is your name?";
+            DefaultName = "No Name";
+            Welcome = "Welcome {0} to your OOP Adventure!";
         }
     }
 }
