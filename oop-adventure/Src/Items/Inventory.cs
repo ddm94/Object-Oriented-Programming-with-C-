a6ad69a299cf5cc15b9fec6a50e146ac501e7d11
Daniele Di Modica <d.dimo94@hotmail.com>
2023-07-21 19:29:05 +0100

    Create Inventory

---
+++ oop-adventure/Src/Items/Inventory.cs
@@ -0,0 +1,22 @@
+﻿
+namespace OOPAdventure
+{
+    public class Inventory
+    {
+        private List<Item> Items { get; } = new();
+
+        // Whenever we access the Total property,
+        // it is the same thing as calling Items.Count
+        public int  Total => Items.Count;
+
+        public void Add(Item item)
+        {
+            Items.Add(item);
+        }
+
+        public void Remove(Item item)
+        {
+            Items.Remove(item);
+        }
+    }
+}
